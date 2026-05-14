0695ceae37794a5f8db9ff2fdb424d85695ad4f6
seonghyeon2471 <carino@hanyang.ac.kr>
2026-05-14 16:58:45 +0900

    Update lidar.py

--- ksh/LIADRMO/lidar.py
+++ ksh/LIADRMO/lidar.py
@@ -1,51 +1,91 @@
-from rplidar import RPLidar
+import serial
+import math
+
 from config import *
 
 class SimpleLidar:
 
     def __init__(self):
 
-        self.lidar = RPLidar(
+        self.ser = serial.Serial(
             LIDAR_PORT,
-            baudrate=LIDAR_BAUD
+            LIDAR_BAUD,
+            timeout=1
         )
-
-        self.iterator = self.lidar.iter_scans()
 
     def read_scan(self):
 
-        scan = next(self.iterator)
-
         points = []
 
-        for (_, angle, distance) in scan:
+        while self.ser.in_waiting >= 47:
 
-            # angle 변환
-            if angle > 180:
-                angle -= 360
+            try:
 
-            # 전방만 사용
-            if angle < ANGLE_MIN:
-                continue
+                # 헤더 찾기
+                b = self.ser.read(1)
 
-            if angle > ANGLE_MAX:
-                continue
+                if b[0] != 0x54:
+                    continue
 
-            # 거리 제한
-            if distance < MIN_LIDAR_DIST:
-                continue
+                data = b + self.ser.read(46)
 
-            if distance > MAX_LIDAR_DIST:
-                continue
+                if len(data) != 47:
+                    continue
 
-            points.append(
-                (angle, distance)
-            )
+                # 패킷 파싱
+                start_angle = (
+                    data[4] |
+                    (data[5] << 8)
+                ) / 100.0
+
+                end_angle = (
+                    data[42] |
+                    (data[43] << 8)
+                ) / 100.0
+
+                if end_angle < start_angle:
+                    end_angle += 360
+
+                angle_step = (
+                    end_angle - start_angle
+                ) / 11.0
+
+                for i in range(12):
+
+                    offset = 6 + i * 3
+
+                    dist = (
+                        data[offset] |
+                        (data[offset + 1] << 8)
+                    )
+
+                    angle = (
+                        start_angle +
+                        angle_step * i
+                    )
+
+                    # -180 ~ 180 변환
+                    if angle > 180:
+                        angle -= 360
+
+                    # 범위 제한
+                    if angle < ANGLE_MIN:
+                        continue
+
+                    if angle > ANGLE_MAX:
+                        continue
+
+                    if dist < MIN_LIDAR_DIST:
+                        continue
+
+                    if dist > MAX_LIDAR_DIST:
+                        continue
+
+                    points.append(
+                        (angle, dist)
+                    )
+
+            except:
+                pass
 
         return points
-
-    def stop(self):
-
-        self.lidar.stop()
-
-        self.lidar.disconnect()
