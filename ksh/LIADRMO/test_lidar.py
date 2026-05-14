43e17b19aa28c067bf483430f3ff245e8d732aae
seonghyeon2471 <carino@hanyang.ac.kr>
2026-05-14 14:14:00 +0900

    Update test_lidar.py

--- ksh/LIADRMO/test_lidar.py
+++ ksh/LIADRMO/test_lidar.py
@@ -1,9 +1,51 @@
-from lidar import SimpleLidar
+from rplidar import RPLidar
+from config import *
 
-lidar = SimpleLidar()
+class SimpleLidar:
 
-while True:
+    def __init__(self):
 
-    scan = lidar.read_scan()
+        self.lidar = RPLidar(
+            LIDAR_PORT,
+            baudrate=LIDAR_BAUD
+        )
 
-    print(len(scan))
+        self.iterator = self.lidar.iter_scans()
+
+    def read_scan(self):
+
+        scan = next(self.iterator)
+
+        points = []
+
+        for (_, angle, distance) in scan:
+
+            # angle 변환
+            if angle > 180:
+                angle -= 360
+
+            # 전방만 사용
+            if angle < ANGLE_MIN:
+                continue
+
+            if angle > ANGLE_MAX:
+                continue
+
+            # 거리 제한
+            if distance < MIN_LIDAR_DIST:
+                continue
+
+            if distance > MAX_LIDAR_DIST:
+                continue
+
+            points.append(
+                (angle, distance)
+            )
+
+        return points
+
+    def stop(self):
+
+        self.lidar.stop()
+
+        self.lidar.disconnect()
