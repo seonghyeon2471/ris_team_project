8bf3897300772e43712e964d226d1db724e9718d
seonghyeon2471 <carino@hanyang.ac.kr>
2026-05-14 18:13:36 +0900

    Update lidar.py

--- ksh/LIADRMO/lidar.py
+++ ksh/LIADRMO/lidar.py
@@ -1,71 +1,46 @@
 from rplidar import RPLidar
 import time
-
 from config import *
 
 class SimpleLidar:
 
     def __init__(self):
 
-        self.lidar = RPLidar(
-            LIDAR_PORT,
-            baudrate=LIDAR_BAUD,
-            timeout=3
-        )
+        self.lidar = RPLidar(LIDAR_PORT, baudrate=LIDAR_BAUD, timeout=3)
 
-        # 안정화
         self.lidar.stop()
-
         time.sleep(1)
 
         self.lidar.reset()
-
         time.sleep(2)
 
         self.lidar.start_motor()
-
         time.sleep(2)
 
-        self.iterator = self.lidar.iter_scans(
-            max_buf_meas=500
-        )
+        self.iterator = self.lidar.iter_scans(max_buf_meas=500)
 
     def read_scan(self):
 
-        raw_scan = next(self.iterator)
+        scan = next(self.iterator)
 
         points = []
 
-        for (_, angle, distance) in raw_scan:
+        for (_, angle, dist) in scan:
 
-            # 0~360 -> -180~180
             if angle > 180:
                 angle -= 360
 
-            # 전방만 사용
-            if angle < ANGLE_MIN:
+            if angle < ANGLE_MIN or angle > ANGLE_MAX:
                 continue
 
-            if angle > ANGLE_MAX:
+            if dist < MIN_LIDAR_DIST or dist > MAX_LIDAR_DIST:
                 continue
 
-            # 거리 제한
-            if distance < MIN_LIDAR_DIST:
-                continue
-
-            if distance > MAX_LIDAR_DIST:
-                continue
-
-            points.append(
-                (angle, distance)
-            )
+            points.append((angle, dist))
 
         return points
 
     def stop(self):
-
         self.lidar.stop()
-
         self.lidar.stop_motor()
-
         self.lidar.disconnect()
