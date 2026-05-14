73ac924b5c6ae037db741ab00b850dccf8790666
seonghyeon2471 <carino@hanyang.ac.kr>
2026-05-14 18:36:20 +0900

    Update main.py

--- ksh/LIADRMO/main.py
+++ ksh/LIADRMO/main.py
@@ -1,12 +1,10 @@
 import time
 
 from lidar import SimpleLidar
-from mapping import LocalMapper
 from planner import GapPlanner
 from motor import MotorController
 
 lidar = SimpleLidar()
-mapper = LocalMapper()
 planner = GapPlanner()
 motor = MotorController()
 
@@ -21,15 +19,12 @@ try:
         if not scan:
             continue
 
-        mapper.update(scan)
-
         v, w = planner.compute_control(scan)
 
         motor.send(v, w)
 
         print(f"v={v:.2f}, w={w:.2f}")
 
-        # 🔥 핵심: sleep 너무 크면 buffer 쌓임
         time.sleep(0.01)
 
 except KeyboardInterrupt:
