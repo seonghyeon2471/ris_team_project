21e33015aa92c725b08aa108d9da97b2dfab643c
seonghyeon2471 <carino@hanyang.ac.kr>
2026-05-14 14:21:42 +0900

    Update lidar.py

--- ksh/LIADRMO/lidar.py
+++ ksh/LIADRMO/lidar.py
@@ -10,6 +10,9 @@ class SimpleLidar:
             baudrate=LIDAR_BAUD
         )
 
+        # 모터 시작
+        self.lidar.start_motor()
+
         self.iterator = self.lidar.iter_scans()
 
     def read_scan(self):
@@ -20,32 +23,29 @@ class SimpleLidar:
 
         for (_, angle, distance) in scan:
 
-            # angle 변환
             if angle > 180:
                 angle -= 360
 
-            # 전방만 사용
             if angle < ANGLE_MIN:
                 continue
 
             if angle > ANGLE_MAX:
                 continue
 
-            # 거리 제한
             if distance < MIN_LIDAR_DIST:
                 continue
 
             if distance > MAX_LIDAR_DIST:
                 continue
 
-            points.append(
-                (angle, distance)
-            )
+            points.append((angle, distance))
 
         return points
 
     def stop(self):
 
         self.lidar.stop()
+
+        self.lidar.stop_motor()
 
         self.lidar.disconnect()
